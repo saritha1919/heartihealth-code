86e4c7ffc26395b90528031db35828239094ccad
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-29 20:17:43 +0530

    Update RunService.bat

--- RunService.bat
+++ RunService.bat
@@ -1,5 +1,1 @@
-net start | find "HeartiHealthservice"
-if %errorlevel% == 2 echo Could not start service.
-if %errorlevel% == 0 echo Service started successfully.
-echo Errorlevel: %errorlevel%
-if ERRORLEVEL 1 sc start "HeartiHealthservice"
+sc start "HeartiHealthservice"
