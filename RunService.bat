d760c6d3e0f257047e47539cea96bb2f32153309
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-29 14:31:33 +0530

    Update RunService.bat

--- RunService.bat
+++ RunService.bat
@@ -1,1 +1,6 @@
-WinSW.NET4.exe install
+@Echo Off
+Echo password@123 > password.txt
+Echo. >> password.txt
+RunAs.exe /netonly /user:PT-RND-001\Saritha "WinSW.NET4.exe install"  < password.txt
+Del password.txt
+
