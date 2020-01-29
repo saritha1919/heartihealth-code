1caac07877c26fcdadc0090a4a62f033eb3352cc
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-29 18:55:07 +0530

    Delete runservice.bat

--- runservice.bat
+++
@@ -1,7 +0,0 @@
-net start | find "myservice2"
-if ERRORLEVEL 1 sc delete "myservice2"
-copy "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services\discoveri-heartihealth-webapp\target\discoveri-heartihealth-webapp-0.0.1-SNAPSHOT.jar" "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services"
-cd "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services"
-start WinSW.NET4.exe install
-sc start "myservice2"
-pause
