90ae8ac82c35ef3d3aef5cad709f6517e622a9bc
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-29 20:30:26 +0530

    Update CreateService.bat

--- CreateService.bat
+++ CreateService.bat
@@ -1,5 +1,5 @@
-net start | find "HeartiHealthservice"
-if ERRORLEVEL 1 sc stop "HeartiHealthservice"
+
+sc stop "HeartiHealthservice"
 sc delete "HeartiHealthservice"
 copy "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services\discoveri-heartihealth-webapp\target\discoveri-heartihealth-webapp-0.0.1-SNAPSHOT.jar" "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services"
 cd "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services"
