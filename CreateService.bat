94b9986f4cd9d9e5b8dda8a7932f15d18fcda559
saritha1919 <47768827+saritha1919@users.noreply.github.com>
2020-01-29 20:20:20 +0530

    Update CreateService.bat

--- CreateService.bat
+++ CreateService.bat
@@ -4,4 +4,3 @@ sc delete "HeartiHealthservice"
 copy "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services\discoveri-heartihealth-webapp\target\discoveri-heartihealth-webapp-0.0.1-SNAPSHOT.jar" "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services"
 cd "C:\Program Files (x86)\Jenkins\workspace\Hearti-Health-Services"
 start WinSW.NET4.exe install
-pause
